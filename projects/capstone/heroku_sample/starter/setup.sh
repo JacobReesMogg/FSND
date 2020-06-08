8bf7a22f9a757cab7560dd3699646e9bb2fc2539
Reptile King <reptile.king@outlook.com>
2020-06-08 18:50:16 +0100

    project set up

---
+++ projects/capstone/heroku_sample/starter/setup.sh
@@ -0,0 +1,5 @@
+# f you're following along in the project, use touch setup.sh and set up all of your environment variables in that file.
+
+# Most of the work we do for Heroku will be in our application files or the command line. In order to give you some familiarity with the web interface, we'll set up the environment variables there, after we deploy our application. For now, check out the screenshot below to get used to the interface. Once you're in a project's settings, you'll see an option to Reveal Config Vars. Once you click on that, a table similar to that you see below will appear. Here, you define your variables just as you did in the setup.sh file, just without the equals signs!
+
+# In the browser, go to your Heroku Dashboard and access your application's settings. Reveal your config variables and start adding all the required environment variables for your project. For the purposes of the sample project, just add one additional one - ‘EXCITED’ and set it to true or false in all lowercase.
